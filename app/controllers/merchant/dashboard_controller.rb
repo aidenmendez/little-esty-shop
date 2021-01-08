c10f584fd9d9c96865df39d757476760306f2a7c
Vanessa Arruda <wanessaarruda@gmail.com>
2021-01-08 14:46:07 -0700

    Delete line from marchant controller

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,13 +1,6 @@
 class Merchant::DashboardController < ApplicationController
 
   def index
-<<<<<<< HEAD
-    # require 'pry'; binding.pry
     @merchant = Merchant.find(params[:merchant_id])
   end
 end
-=======
-    @merchant = Merchant.find(params[:merchant_id])
-  end
-end
->>>>>>> 74047d40c3af3c61300bce2e90b40d4455b0324c
