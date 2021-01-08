4b1f54d6bf455dfbdcc2703474384c4c0e4f2986
Aiden Mendez <aidenmendez@gmail.com>
2021-01-08 16:04:59 -0700

    Add empty it block to merchant dashboard test

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -4,12 +4,25 @@ RSpec.describe "as a merchant" do
   describe "when I visit the merchant dashboard" do
     before(:each) do
       @merchant = Merchant.create!(name: "Buy more stuff")
+      @merchant = Merchant.create!(name: 'House of thingys')
+      @smith = Customer.create!(first_name: 'Smith', last_name:"John")
+      @doe = Customer.create!(first_name: 'Smith', last_name:"John")
+      @doe = Customer.create!(first_name: 'Smith', last_name:"John")
+      @doe = Customer.create!(first_name: 'Smith', last_name:"John")
+      @doe = Customer.create!(first_name: 'Smith', last_name:"John")
+      @doe = Customer.create!(first_name: 'Smith', last_name:"John")
+      @doe = Customer.create!(first_name: 'Smith', last_name:"John")
+      @invoice_1 = Invoice.create!(customer_id: @customer.id, merchant_id: @merchant.id, status: 0, created_at: 2012-03-25)
+      @invoice_2 = Invoice.create!(customer_id: @customer.id, merchant_id: @merchant.id, status: 0)
+      @invoice_3 = Invoice.create!(customer_id: @customer.id, merchant_id: @merchant.id, status: 1)
+
     end
     it "Then I see the name of my merchant" do
       visit merchant_dashboard_index_path(@merchant.id)
 
       expect(page).to have_content("#{@merchant.name}")
     end
+
     it "dashboard links to my merchant items index" do
       visit merchant_dashboard_index_path(@merchant.id)
 
@@ -19,6 +32,7 @@ RSpec.describe "as a merchant" do
 
       expect(current_path).to eq(merchant_items_path(@merchant.id))
     end
+
     it "dashboard links to my merchant invoices index" do
       visit merchant_dashboard_index_path(@merchant.id)
 
@@ -28,6 +42,9 @@ RSpec.describe "as a merchant" do
 
       expect(current_path).to eq("/merchants/#{@merchant.id}/invoices")
     end
-    
+
+    it "I see the my merchant's top five customers and their respective number of successful transactions with my merchant" do
+      # expect(page).to have_content()
+    end
   end
 end
