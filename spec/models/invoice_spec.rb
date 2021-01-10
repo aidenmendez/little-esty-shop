40ddb32cfb1cdc850eb97a4a26173ee2f8a302ed
awosborn <alanturing@example.coclear>
2021-01-10 14:15:09 -0700

    Add dependent destroys to merchant model, fix test

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -12,12 +12,14 @@ RSpec.describe Invoice, type: :model do
 
   describe 'class methods' do
     it "shows incomplete invoices" do
+      Invoice.destroy_all
+
     merchant = Merchant.create!(name: 'House of thingys')
     customer_1 = Customer.create!(first_name: 'John', last_name: 'Doe')
     invoice_1 = Invoice.create!(customer_id: customer_1.id, merchant_id: merchant.id, status: 0)
     invoice_2 = Invoice.create!(customer_id: customer_1.id, merchant_id: merchant.id, status: 1)
     invoice_3 = Invoice.create!(customer_id: customer_1.id, merchant_id: merchant.id, status: 2)
-
+  
     expect(Invoice.incomplete_invoices).to eq([invoice_1.id])
     end
   end
