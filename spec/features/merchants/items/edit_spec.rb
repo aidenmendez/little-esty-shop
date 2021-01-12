ad72516e8cb3b0c703a720ebf10d24727b8783df
Aiden Mendez <aidenmendez@gmail.com>
2021-01-11 17:25:09 -0700

    Complete merchant item update story

--- spec/features/merchants/items/edit_spec.rb
+++ spec/features/merchants/items/edit_spec.rb
@@ -16,11 +16,22 @@ RSpec.describe "As a merchant" do
 
     it "I see a form filled in with the existing item attribute information" do
       visit edit_merchant_item_path(@item1.merchant.id, @item1.id )
-   
+
+      expect(page).to have_field("Name", with: @item1.name)
+      expect(page).to have_field("Description", with: @item1.description)
+      expect(page).to have_field("Price", with: @item1.unit_price)
     end
 
-    it "When I update the information in the form and I click ‘submit’, I'm redirected back to the item show page where I see the updated information" do
+    it "When I update the information in the form and I click ‘submit’, I'm redirected back to the item show page where I see the updated information and I see a flash message stating that the information has been successfully updated." do
+      visit edit_merchant_item_path(@item1.merchant.id, @item1.id )
+      updated_price = 22
+      fill_in "Price", with: updated_price
+      click_on "Update"
 
+      expect(current_path).to eq(merchant_item_path(@item1.merchant_id, @item1.id))
+
+      expect(page).to have_content("Current Price: #{updated_price}" )
+      # expect(flash[:notice]).to eq("Item details updated successfully")
     end
   end
 end
