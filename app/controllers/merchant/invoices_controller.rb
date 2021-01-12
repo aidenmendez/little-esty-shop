13d02e2a27e53a71658f5a0b002c490de44b46d9
Aiden Mendez <aidenmendez@gmail.com>
2021-01-12 15:43:12 -0700

    Fix more errors

--- app/controllers/merchant/invoices_controller.rb
+++ app/controllers/merchant/invoices_controller.rb
@@ -7,5 +7,14 @@ class Merchant::InvoicesController < ApplicationController
   def show
     @merchant = Merchant.find(params[:merchant_id])
     @invoice = Invoice.find(params[:id])
+    @customer = Customer.find(@invoice.customer_id)
+    @items = @invoice.items.to_a
+    @item_data = @items.each_with_object({}) do |item, memo|
+      memo[item.id] = {
+        quantity: InvoiceItem.find_by(invoice_id: @invoice.id, item_id: item.id).quantity,
+        unit_price: InvoiceItem.find_by(invoice_id: @invoice.id, item_id: item.id).unit_price,
+        status: InvoiceItem.find_by(invoice_id: @invoice.id, item_id: item.id).status
+      }
+    end
   end
 end
