ba97e2cf13521bc18c6dc9083dbe238a4c01f793
Aiden Mendez <aidenmendez@gmail.com>
2021-01-12 12:21:12 -0700

    Complete invoice item details story

--- app/controllers/merchant/invoices_controller.rb
+++ app/controllers/merchant/invoices_controller.rb
@@ -9,8 +9,12 @@ class Merchant::InvoicesController < ApplicationController
     @invoice = Invoice.find(params[:id])
     @customer = Customer.find(@invoice.customer_id)
     @items = @invoice.items.to_a
-    @item_quants = @items.each_with_object({}) do |item, memo|
-      memo[item.id] = InvoiceItem.find_by(invoice_id: @invoice.id, item_id: item.id).quantity
+    @item_data = @items.each_with_object({}) do |item, memo|
+      memo[item.id] = {
+        quantity: InvoiceItem.find_by(invoice_id: @invoice.id, item_id: item.id).quantity,
+        unit_price: InvoiceItem.find_by(invoice_id: @invoice.id, item_id: item.id).unit_price,
+        status: InvoiceItem.find_by(invoice_id: @invoice.id, item_id: item.id).status
+      }
     end
   end
 end
